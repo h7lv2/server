03296a52abae2c35fd059790efee0014b44765d1
halva <xolagix@outlook.com>
2026-01-01 19:08:56 +0300

    switched to cloudflare as provider

--- certbot-entrypoint.sh
+++ certbot-entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
-echo "dns_porkbun_key = $PORKBUN_KEY" > /porkbun.ini
-echo "dns_porkbun_secret = $PORKBUN_SECRET" >> /porkbun.ini
-chmod 600 /porkbun.ini
+echo "dns_cloudflare_api_token = $CLOUDFLARE_API_TOKEN" > /cloudflare.ini
+chmod 600 /cloudflare.ini
 
 for DOMAIN in $(env | grep '^DOMAIN_' | cut -d= -f2); do
     if [ ! -d "/etc/letsencrypt/live/$DOMAIN" ]; then
         STAGING_FLAG=$([ "$STAGING" = "true" ] && echo "--staging" || echo "")
-        certbot certonly --authenticator dns-porkbun \
-            --dns-porkbun-credentials /porkbun.ini \
-            --dns-porkbun-propagation-seconds 60 \
+        certbot certonly --authenticator dns-cloudflare \
+            --dns-cloudflare-credentials /cloudflare.ini \
+            --dns-cloudflare-propagation-seconds 60 \
             $STAGING_FLAG -d "$DOMAIN" -d "*.$DOMAIN" \
             --email "$EMAIL" --agree-tos --no-eff-email --non-interactive
     fi
